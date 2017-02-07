68d91294f57385b91ba5e02774f531161cc37890
mhussien86 <m.hussien.android@gmail.com>
2017-02-07 22:30:36 +0200

    Creating main activity and main activity xml file

---
+++ app/src/main/java/com/friendsurance/weather/app/ui/MainActivity.java
@@ -0,0 +1,21 @@
+package com.friendsurance.weather.app.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.friendsurance.weather.app.R;
+
+/**
+ * Created by mohamed on 07/02/17.
+ */
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+
+    }
+}
---
+++ app/src/main/java/com/friendsurance/weather/app/ui/listofcities/ListOfCitiesFragment.java
@@ -0,0 +1,7 @@
+package com.friendsurance.weather.app.ui.listofcities;
+
+/**
+ * Created by mohamed on 07/02/17.
+ */
+public class ListOfCitiesFragment {
+}
