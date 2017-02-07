40060977d383666354a2a5711b545903181a77eb
mhussien86 <m.hussien.android@gmail.com>
2017-02-08 01:14:06 +0200

    Adding page indicator classes

--- app/src/main/java/com/friendsurance/weather/app/ui/listofcities/ListOfCitiesFragment.java
+++ app/src/main/java/com/friendsurance/weather/app/ui/listofcities/ListOfCitiesFragment.java
@@ -1,7 +1,9 @@
 package com.friendsurance.weather.app.ui.listofcities;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Created by mohamed on 07/02/17.
  */
-public class ListOfCitiesFragment {
+public class ListOfCitiesFragment extends Fragment {
 }
